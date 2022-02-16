ae1d882e8b1b56831e73717fe397e40b1bc73dee
Juliana Pessoa Panini <86628688+juhlianna@users.noreply.github.com>
2022-02-16 14:45:41 -0300

    Desafio.Java

---
+++ Desafio.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner input =new Scanner(System.in);
+    int b;
+    char c;
+    int d=0,e=0,f=0,i,t;
+    int a=input.nextInt();
